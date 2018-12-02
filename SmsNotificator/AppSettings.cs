aac131656461a9b762a4663d0e10bb75c736a48d
fairendil <fairendil@gmail.com>
2018-12-02 10:14:44 +0300

    Init

---
+++ SmsNotificator/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace SmsNotificator
+{
+    public class AppSettings
+    {
+        public string AppToken { get; set; }
+        public int Version { get; set; }
+    }
+}
---
+++ SmsNotificator/Controllers/SMSNotificationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmsNotificator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SMSNotificationController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet("{text}")]
+        public ActionResult<string> SendSMS(string text)
+        {
+            var parameters = new NameValueCollection {
+                { "token", "az4mwwzjyeephpmwfom8kyk6u72dc1" },
+                { "user", "ues8y1vvw4i24a34mtcnhnzaczpnui" },
+                { "message", text }
+            };
+
+            using (var client = new WebClient())
+            {
+                client.UploadValues("https://api.pushover.net/1/messages.json", parameters);
+            }
+
+            return text;
+        }
+    }
+}
